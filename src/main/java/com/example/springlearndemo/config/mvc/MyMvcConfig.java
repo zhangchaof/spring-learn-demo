2615678fb3d3fca8b5dfebcb5dffc8e771d119de
chaofan.zhang <chaofan.zhang@zhangmen.com>
2020-05-23 10:02:06 +0800

    解析修改

--- src/main/java/com/example/springlearndemo/config/mvc/MyMvcConfig.java
+++ src/main/java/com/example/springlearndemo/config/mvc/MyMvcConfig.java
@@ -13,15 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * ps:
- * 在Spring Boot 2.0之前版本都是靠重写 WebMvcConfigurerAdapter 的方法来添加自定义拦截器，消息转换器等。
- * SpringBoot 2.0 后，该类被标记为@Deprecated。现在，我们只能靠实现 WebMvcConfigurer 接口来实现了
- *
- * 配置 @EnableWebMvc 注解并实现 WebMvcConfigurer 接口之后，启动项目发现 css, js 等这些静态资源404了，网上搜集到的引起原因如下：
- * 用户配置了@EnableWebMvc
- * Spring扫描所有的注解，再从注解上扫描到@Import，把这些@Import引入的bean信息都缓存起来
- * 在扫描到@EnableWebMvc时，通过@Import加入了 DelegatingWebMvcConfiguration，也就是WebMvcConfigurationSupport
- * spring在处理@Conditional相关的注解，判断发现已有WebMvcConfigurationSupport，就跳过了spring bootr的WebMvcAutoConfiguration
- * 所以spring boot自己的静态资源配置不生效。
+ * SpringBoot对SpringMVC的自动配置不需要了，所有都是我们自己配置；所有的SpringMVC的自动配置都失效了,我们需要在配置类中添加@EnableWebMvc即可；
+ * 原理：WebMvcAutoConfiguration里面@ConditionalOnMissingBean({WebMvcConfigurationSupport.class})才生效，
+ *      而@EnableWebMvc导入了 @Import({DelegatingWebMvcConfiguration.class}) ->DelegatingWebMvcConfiguration extends WebMvcConfigurationSupport
  *
  * 解决方式为：
  * （一）注释或删除掉@EnableWebMvc注解
